3e6a42f1085ecc2a98a769b640b236cf1491931b
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-17 16:26:25 +0300

    get more function

--- scratch/src/app.controller.ts
+++ scratch/src/app.controller.ts
@@ -4,8 +4,13 @@ import { Controller, Get } from "@nestjs/common";
 @Controller()
 export class AppController{
 
-    @Get()
+    @Get('/asdf')
     getRootRoute(){
         return  'hi there!'
+    }
+
+    @Get('/bye')
+    getByeThere(){
+        return 'bye there!'
     }
 }
