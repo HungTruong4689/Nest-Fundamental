3f7e4a9413eb38271ac48f3762aeacf1c0b5c1a5
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-18 15:57:56 +0300

    more di service

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -4,6 +4,12 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
     constructor(private powerService: PowerService){
-        
+
+    }
+
+    compute(a:number, b:number){
+        console.log('Drwaing 10 watts of power from Power Service');
+        this.powerService.supplyPower(10);
+        return a+b
     }
 }
